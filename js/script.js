34bdc760c5fd7c7440e6196f4c0de690220a1859
saeedeh <s.saldorgar95@gmail.com>
2024-02-10 11:59:51 +0330

    Continuation of js codes

--- js/script.js
+++ js/script.js
@@ -11,9 +11,48 @@ colorsBox.forEach( function(colorbox){
   let mainColor = event.target.style.backgroundColor 
   inputElem.style.backgroundColor = mainColor
  })
+
 })
 
+function generateNewNote(){
+
+    let newNoteDivElem = $.createElement('div')
+    newNoteDivElem.className = 'card shadow-sm rounded'
+    let inputBg = inputElem.style.backgroundColor
+    newNoteDivElem.style.backgroundColor = inputBg
+    newNoteDivElem.addEventListener('click' , removeNote)
+
+    let newNotePElem = $.createElement('p')
+    newNotePElem.className = 'card-text p-3'
+    newNotePElem.innerHTML = inputElem.value
+
+    newNoteDivElem.append(newNotePElem)
+
+    notesContainer.append(newNoteDivElem)
+
+    inputElem.value = ''
+    inputElem.style.backgroundColor = '#fff'
+
+}
+
+function removeNote(event){
+    event.target.parentElement.remove()
+}
+
 btnDeleteElem.addEventListener('click', function(){
+
     inputElem.value = ''
     inputElem.style.backgroundColor = "#fff"
 })
+
+inputElem.addEventListener('keydown', function(event){
+
+    if(event.keyCode === 13){
+      if(inputElem.value){
+         generateNewNote()
+         }
+    }  
+    
+})
+
+btnSaveElem.addEventListener('click', generateNewNote)
